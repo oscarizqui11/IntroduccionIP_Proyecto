e505aa0cf8ac8976a5ecc5281332400ee07578ca
patrimontecino <69516877+patrimontecino@users.noreply.github.com>
2020-08-19 18:00:25 +0200

    Add files via upload

---
+++ src/Fondo.js
@@ -0,0 +1,72 @@
+import React from "react";
+import bcn from "url:./bcn.png";
+import "./styleDistrito.css";
+
+
+
+let styleFondo = {
+  width: '100%',
+
+  backgroundImage: {bcn},
+  backgroundRepeat: 'noRepeat',
+  backgroundSize: '100%',
+  opacity: '0.4',
+  position: 'absolute'
+
+}
+
+class Fondo extends React.Component {
+  render() {
+    return (
+
+      <img style={styleFondo} src={bcn}>
+
+      </img>
+    );
+  }
+}
+
+export default Fondo; 
+/*
+
+let styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: null,
+  }
+});
+
+class Fondo extends React.Component {
+  render() {
+    return (
+<Image style={styles.backgroundImage}>
+  <View>
+    <Text>TODO LO DEMAS</Text>
+  </View>
+</Image>
+    )}}; 
+
+
+export default Fondo; 
+
+
+//OTRA OPCION
+
+let styleFondo={  
+    backgroundImage: "url("+"{bcn}"+")",
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
+class Fondo extends Component {
+  render() {
+    return (
+      <section style={styleFondo}>
+      </section>
+    );
+  }
+}
+
+*/
